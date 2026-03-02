4b0dec96887befdb87a5e8ca667fd5ecfcfb477f
kajzatullin <kajzatullin>
2026-03-02 09:21:50 +0300

    создание проекта4

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/название-вашего-репозитория/', // Важно: слеши с двух сторон!
+  base: '/auto-frontend/',
 })
